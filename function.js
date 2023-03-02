6f01a8c247a1f721470d9a6693ad747d6dc2505f
gabrieldelpacheco182 <gabriel_o_pacheco@estudante.sesisenai.org.br>
2023-03-02 19:17:11 -0300

    combinando 2 array.

---
+++ function.js
@@ -0,0 +1,4 @@
+const usuarios = ['Pedro', 'José', 'Aderbal', 'Danilo', 'Luisa', 'Vitoria'];
+const frutas = [ 'Banana', 'Morango', 'Maçã', 'Uva', 'Pêra' , 'Laranja'];
+const novoArray = usuarios.concat(frutas);
+console.log(novoArray);
